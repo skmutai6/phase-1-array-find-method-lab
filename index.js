5d7800e2ed006b8727691dde02febaec47fb4696
skmutai6 <skmutai6@gmail.com>
2024-03-25 22:44:49 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+const records = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+const superbowlWin = records.find((record) => {
+    return record.result == "W"
+})
+
+console.log(superbowlWin);
